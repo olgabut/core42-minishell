57f3c087ece0ce579688c78f712a6911e945b11b
Niel <dyprih@gmail.com>
2026-03-28 13:47:52 +0100

    add functions to handle redirections in child, preparing pipes for here_doc and funticon for writing here_doc string to pipe in parent

--- solution/executor/redirection.c
+++ solution/executor/redirection.c
@@ -6,15 +6,15 @@
 /*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 13:17:20 by dprikhod          #+#    #+#             */
-/*   Updated: 2026/03/27 16:15:21 by dprikhod         ###   ########.fr       */
+/*   Updated: 2026/03/28 13:43:35 by dprikhod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "executor/redirection.h"
 #include "minishell.h"
+#include <fcntl.h>
+#include <errno.h>
 
-/*
- * Should create pipes for any here_doc needed.
- */
 int	**prepare_pipes_for_here_doc(t_cmd *cmd)
 {
 	t_io	*list;
@@ -46,10 +46,6 @@ int	**prepare_pipes_for_here_doc(t_cmd *cmd)
 	return (ret);
 }
 
-/*
-* Write here_doc's 'line' to the pipe and close both ends of this pipe. Repeat 
-* for every here_doc token.
-*/
 int	fill_here_doc(t_cmd *cmd, int *fd[2])
 {
 	t_io	*list;
@@ -64,6 +60,48 @@ int	fill_here_doc(t_cmd *cmd, int *fd[2])
 			close(*fd[1]);
 			fd++;
 		}
+	}
+	return (0);
+}
+
+int	apply_here_doc(int fd[2])
+{
+	close(fd[1]);
+	dup2(fd[0], STDIN_FILENO);
+	return (0);
+}
+
+int	prepare_redirs_before_exec(t_cmd *cmd, int *hd_pipe[2])
+{
+	t_io	*list;
+	int		new_fd;
+	int		old_fd;
+
+	list = cmd->io_list;
+	old_fd = STDIN_FILENO;
+	while (list->next)
+	{
+		if (list->type == TOKEN_REDIR_IN)
+			new_fd = open(list->path, O_RDONLY);
+		else if (list->type == TOKEN_REDIR_OUT)
+			new_fd = open(list->path, O_WRONLY | O_TRUNC);
+		else if (list->type == TOKEN_APPEND)
+			new_fd = open(list->path, O_WRONLY | O_TRUNC | O_APPEND);
+		else if (list->type == TOKEN_HEREDOC)
+		{
+			if (!hd_pipe)
+				return (1);
+			close(*hd_pipe[1]);
+			new_fd = *hd_pipe[0];
+			hd_pipe++;
+		}
+		if (new_fd < 0)
+			return(errno);
+		if (list->type == TOKEN_REDIR_OUT || list->type == TOKEN_APPEND)
+			old_fd = STDOUT_FILENO;
+		if (dup2(new_fd, old_fd) < 0)
+			return (errno);
+		list = list->next;
 	}
 	return (0);
 }
---
+++ solution/inc/executor/redirection.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/28 10:39:02 by dprikhod          #+#    #+#             */
+/*   Updated: 2026/03/28 13:38:53 by dprikhod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REDIRECTION_H
+#define REDIRECTION_H
+
+#include "minishell.h"
+
+/*
+ * Creates pipes for any here_doc needed.
+ */
+int	**prepare_pipes_for_here_doc(t_cmd *cmd);
+
+/*
+* Writes here_doc's 'line' to the pipe and closes both ends of this pipe. Repeat 
+* for every here_doc token.
+*/
+int	fill_here_doc(t_cmd *cmd, int *fd[2]);
+
+/*
+* Opens file with name `cmd->io_list->filename` with read-only or write-only
+* permissions (and other flags), depending on `cmd->io_list->type`. Pass
+* created file descriptor to `dup2` function call to redirect output or input
+* accordingly.
+*
+*
+* For `TOKEN_HERE_DOC` redirection is made to `*hd_pipe[0]`, `*hd_pipe[1]` is
+* closed as well. 
+*
+* `int *hd_pipe[2]` is array of "pipes" created by `prepare_pipes_for_here_doc`
+* function for every `TOKEN_HERE_DOC` token in `cmd->io_list`
+*/
+int	prepare_redirs_before_exec(t_cmd *cmd, int *hd_pipe[2]);
+
+#endif
