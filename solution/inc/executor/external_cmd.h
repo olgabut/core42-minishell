6676497982c1f500daf266b65b558005870bde7c
solsarok <solsarok@gmail.com>
2026-04-14 13:21:58 +0200

    Fix function name external_child_process

--- solution/inc/executor/external_cmd.h
+++ solution/inc/executor/external_cmd.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   external_cmd.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dprikhod <dprikhod@student.42.fr>          +#+  +:+       +#+        */
+/*   By: obutolin <obutolin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 17:20:49 by dprikhod          #+#    #+#             */
-/*   Updated: 2026/04/10 01:48:46 by dprikhod         ###   ########.fr       */
+/*   Updated: 2026/04/14 13:11:13 by obutolin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 # include "executor/execute.h"
 # include "minishell.h"
 
-void	external_child(t_exec_info *ei);
+void	external_child_process(t_exec_info *ei);
 /*
 * # RETURN VALUE
 *
